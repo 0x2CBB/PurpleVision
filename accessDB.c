80962c80334fb0402319791e77c7d60d30d10029
0x2CB <107046146+0x2CBB@users.noreply.github.com>
2023-01-05 19:32:42 +0100

    Update accessDB.c

--- accessDB.c
+++ accessDB.c
@@ -3,7 +3,7 @@
 
 static int callback(void *data, int argc, char **argv, char **azColName){
    int i;
-   fprintf(stderr, "%s: ", (const char*)data);
+   fprintf(stderr, "%s", (const char*)data);
    
    for(i = 0; i<argc; i++){
       printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
